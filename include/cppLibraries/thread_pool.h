efb91ef7bbd2cd2518a8490e6ff63a0a58e5af0b
Shaan-somaiah <shaansomaiah@gmail.com>
2026-07-29 19:32:52 +0530

    Boiler plate for thread pool class

---
+++ include/cppLibraries/thread_pool.h
@@ -0,0 +1,13 @@
+#ifndef CPP_LIBRARIES_THREAD_POOL_H
+#define CPP_LIBRARIES_THREAD_POOL_H
+
+
+namespace cppLibraries {
+
+    class ThreadPool {
+
+    };
+
+} // namespace cppLibraries
+
+#endif //CPP_LIBRARIES_THREAD_POOL_H
