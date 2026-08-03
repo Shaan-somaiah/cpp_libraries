ffa9bbb95c4d50f444d4b06c386acf0d12499a99
Shaan-somaiah <shaansomaiah@gmail.com>
2026-08-03 13:58:37 +0530

    Basic structure of header file

--- include/cppLibraries/thread_pool.h
+++ include/cppLibraries/thread_pool.h
@@ -1,10 +1,34 @@
 #ifndef CPP_LIBRARIES_THREAD_POOL_H
 #define CPP_LIBRARIES_THREAD_POOL_H
 
+#include <functional>
 
 namespace cppLibraries {
 
     class ThreadPool {
+
+        private: 
+            int m_worker_count;
+
+            struct m_work {
+                std::function<void()> m_callback;
+                int m_priority;
+                int task_id;
+                
+            };
+
+            // Need to create a DS library to implement basic LL featureset
+            // LinkedList<m_work> m_queue;
+
+            void WorkerLoop();
+
+        public:
+            ThreadPool() = delete;
+            ThreadPool(int workercount);
+
+            ~ThreadPool();
+
+            
 
     };
 
