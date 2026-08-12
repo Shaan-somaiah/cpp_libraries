db0e6ca746f89efc51ddcad1d5648cbf810f1c4d
Shaan-somaiah <shaansomaiah@gmail.com>
2026-08-12 20:27:41 +0530

    Implement PushFront()

--- include/cppLibraries/data_structures/double_linked_list.h
+++ include/cppLibraries/data_structures/double_linked_list.h
@@ -49,6 +49,24 @@ template <typename T> ds::DoubleLinkedList<T>::~DoubleLinkedList() {
 
 template <typename T> void ds::DoubleLinkedList<T>::PushFront(T data) {
     
+    Node* tmpptr = new Node;
+
+    // first node
+    if(m_length == 0) {
+        tmpptr->m_data = data;
+        m_head = tmpptr;
+        m_tail = tmpptr;
+        tmpptr->m_next = nullptr;
+        tmpptr->m_prev = nullptr;
+    }
+
+    tmpptr->m_next = m_head;
+    m_head->m_prev = tmpptr;
+    m_head = tmpptr;
+
+    m_length++;
+
+    free(tmpptr);
 }
 
 template <typename T> void ds::DoubleLinkedList<T>::PushBack(T data) {
@@ -64,10 +82,10 @@ template <typename T> T ds::DoubleLinkedList<T>::PopBack() {
 }
 
 template <typename T> size_t ds::DoubleLinkedList<T>::GetLength() {
-    
+    return m_length;
 }
 template <typename T> bool ds::DoubleLinkedList<T>::Exists(T data) {
-    
+    return false;
 }
 
 
